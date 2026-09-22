6955bcca6bdefd7aee587874fc9232a6c46c0153
INJI-CANI <injij@dasaron.com>
2026-09-22 15:06:37 +0900

    Update Tables.jsx

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { CircleDot, Check, Trash2, RefreshCw } from "lucide-react";
 import {
   supabase,
